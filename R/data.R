f44a516eb18b8155cbfe30adbb72216985163dc3
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-09-12 10:41:48 -0700

    Add sim_vec and help.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+##' Simulated vector of 1000 simulated values from a power-law distribution
+##'
+##' As used for Figures 1 and 2 of MEE paper, used as a default set of
+##' values. Paramaters are `b = -2`, `xmin = 1`, xmax = 1000`, and the seed was
+##' set to 42.
+##'
+##' @format numeric
+##' @examples
+##' \dontrun{
+##' head(sim_vec)
+##' }
+##' @author Andrew Edwards
+##' @source Generated from `data-raw/sim_vec.R`.
+"sim_vec"
