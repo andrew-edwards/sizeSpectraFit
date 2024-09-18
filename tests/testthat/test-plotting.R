75db1c12298a37c86b7e555562c76f90c7e8092e
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-09-18 10:21:27 -0700

    More test for different options in add_ticks_to_one_axis().

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -8,8 +8,20 @@ test_that("plot.size_spectrum_numeric() works", {
   expect_invisible(plot(res_vec, log = "y"))
   expect_invisible(plot(res_vec, log = ""))
   expect_invisible(plot(res_vec,
-                        x_small_ticks_labels = c(5, 50, 500,5000),
+                        x_small_ticks_labels = c(5, 50, 500, 5000)))
+  expect_invisible(plot(res_vec,
+                        x_small_ticks_labels = c(5, 50, 500, 5000),
                         log = "y"))
   expect_invisible(plot(res_vec,
-                        x_small_ticks_labels = c(5, 50, 500,5000)))
+                        log = "y",
+                        x_big_ticks = c(5, 50, 500, 5000)))
+  expect_invisible(plot(res_vec,
+                        log = "y",
+                        x_small_ticks = c(5, 50, 500)))
+  expect_invisible(plot(res_vec,
+                        log = "y",
+                        x_small_ticks_by = 25))
+  expect_invisible(plot(res_vec,
+                        log = "y",
+                        x_small_ticks_by = NA))
 })
