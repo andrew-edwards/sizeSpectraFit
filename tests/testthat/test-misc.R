d1b6f8c26c1fbde7e13b67235473fcf8dec68a2b
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-04-21 09:39:44 -0700

    Rename test file and do test for f().

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -1,8 +1,10 @@
-# Test helper function a()
+# Test helper functions a() and f()
 
 test_that("a() works on a tibble", {
   expect_equal(tibble::tibble(x = 1:50, y = 51:100) %>% a(),
                as.data.frame(tibble::tibble(x = 1:50, y = 51:100)))
 
-
+test_that("f() works as expected", {
+  expect_equal(f(123456),
+               "123,456")
 })
