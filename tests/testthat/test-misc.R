1473e229c7bb8862ccc01fc212d031eb919b4876
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-04-21 09:55:23 -0700

    Fix brackets.

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -3,6 +3,7 @@
 test_that("a() works on a tibble", {
   expect_equal(tibble::tibble(x = 1:50, y = 51:100) %>% a(),
                as.data.frame(tibble::tibble(x = 1:50, y = 51:100)))
+})
 
 test_that("f() works as expected", {
   expect_equal(f(123456),
